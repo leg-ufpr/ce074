20cdb866816f3a1a73774b88379151c31dab7090
Fernando Mayer <fernandomayer@gmail.com>
2018-08-06 18:49:19 -0300

    Latex em nonstopmode

--- slides-pdf/_build_all.sh
+++ slides-pdf/_build_all.sh
@@ -11,13 +11,13 @@ for RNW in $RNWFILES; do
     FILE="${RNW%.*}"
     echo $FILE
     Rscript -e "require(knitr); knit(\"$RNW\")"
-    pdflatex $FILE
+    pdflatex -interaction=nonstopmode -halt-on-error $FILE
     if grep -q '\addbibresource{.*}' "$RNW"; then
         biber $FILE
-        pdflatex $FILE
+        pdflatex -interaction=nonstopmode -halt-on-error $FILE
     fi
-    pdflatex $FILE
-    pdflatex $FILE
+    pdflatex -interaction=nonstopmode -halt-on-error $FILE
+    pdflatex -interaction=nonstopmode -halt-on-error $FILE
 done
 
 rm *.aux *.log *.nav *.out *.snm *.tex *.toc *.bbl *.bcf *.blg *.run.xml *.vrb
